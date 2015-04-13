2ac3acdabf57f81926b30d57255501924b321532
Thomas Steinhauer <thomassteinhauer@gmail.com>
2015-04-13 14:18:17 +0200

    init

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Interface with the Salesforce API by wrapping npm package JSforce for meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/strikeout/meteor-salesforce',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
