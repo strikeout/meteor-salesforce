3ee3c03193803cb390da20c2e428ce2a2b6c01b9
Ben de Laporte <accs-github@gchameleon.tk>
2016-11-17 20:10:15 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'strikeout:salesforce',
-  version: '1.4.1',
+  name: 'gchameleon:salesforce',
+  version: '1.4.2',
   // Brief, one-line summary of the package.
   summary: 'Salesforce for Meteor (via JSforce)',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/strikeout/meteor-salesforce',
+  git: 'https://github.com/gchameleon/meteor-salesforce',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
